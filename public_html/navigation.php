cb394bd3e7971fbd99ec6958c49b6cefef8ae14a
DESKTOP-EVOSS0N\DeVaughn Scott <dc.scott@my.simpson.edu>
2017-11-07 09:31:17 -0600

    Changed navigation to direct towards .php instead of html

--- public_html/navigation.php
+++ public_html/navigation.php
@@ -1,10 +1,10 @@
 <ul class="nav navbar-nav">
-          <li><a href="index.html">Home Page</a></li>
-          <li class="active"><a href="about.html">About</a></li>
-          <li><a href="news.html">News Page</a></li>
-          <li><a href="sports.html">Sports Page</a></li>
-          <li><a href="tour_guide.html">Tour Guide Page</a></li>
-          <li><a href="history.html">History Page</a></li>
-          <li><a href="login.html">Log In</a></li>
-          <li><a href="sign_up.html">Sign Up</a></li>
+          <li><a href="index.php">Home Page</a></li>
+          <li class="active"><a href="about.php">About</a></li>
+          <li><a href="news.php">News Page</a></li>
+          <li><a href="sports.php">Sports Page</a></li>
+          <li><a href="tour_guide.php">Tour Guide Page</a></li>
+          <li><a href="history.php">History Page</a></li>
+          <li><a href="login.php">Log In</a></li>
+          <li><a href="sign_up.php">Sign Up</a></li>
         </ul>
